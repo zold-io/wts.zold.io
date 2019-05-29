2cdc64c391cd2e711db24e0cf6648947144a97cc
Yegor Bugayenko <yegor256@gmail.com>
2019-05-29 09:49:46 +0300

    #248 show tx fee for btc

--- front/front_rate.rb
+++ front/front_rate.rb
@@ -48,6 +48,7 @@ settings.daemons.start('snapshot', 24 * 60 * 60) do
   distributed = Zold::Amount.new(
     zents: (settings.ticks.latest('Emission') - settings.ticks.latest('Office')).to_i
   )
+  sibit = Sibit.new(log: settings.log, attempts: 4)
   settings.telepost.spam(
     [
       "Today is #{Time.now.utc.strftime('%d-%b-%Y')} and we are doing great:\n",
@@ -57,8 +58,9 @@ settings.daemons.start('snapshot', 24 * 60 * 60) do
       "  Distributed: [#{distributed}](https://wts.zold.io/rate)",
       "  24-hours volume: [#{settings.gl.volume}](https://wts.zold.io/gl)",
       "  24-hours txns count: [#{settings.gl.count}](https://wts.zold.io/gl)",
-      "  Nodes: [#{settings.ticks.latest('Nodes')}](https://wts.zold.io/remotes)",
+      "  Nodes: [#{settings.ticks.latest('Nodes').round}](https://wts.zold.io/remotes)",
       "  Bitcoin price: $#{price.round}",
+      "  Bitcoin tx fee: $#{format('%.02f', sibit.fees[:XL] * 250 / 100_000_000)}",
       "  Rate: [#{format('%.08f', rate)}](https://wts.zold.io/rate) ($#{(price * rate).round(2)})",
       "  Coverage: [#{format('%.08f', coverage)}](https://wts.zold.io/rate) \
 / [#{(100 * coverage / rate).round}%](http://papers.zold.io/fin-model.pdf)",
