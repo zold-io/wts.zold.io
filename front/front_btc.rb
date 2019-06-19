9d707925c0c2f79ce0e18c640f7870b075c064ed
Yegor Bugayenko <yegor256@gmail.com>
2019-06-19 18:46:42 +0300

    #266 typo

--- front/front_btc.rb
+++ front/front_btc.rb
@@ -80,7 +80,7 @@ unless ENV['RACK_ENV'] == 'test'
       settings.telepost.spam(
         "⚠️ Our Bitcoin Blockchain monitoring system is behind, for #{diff} blocks!",
         "The most recently seen block is [#{seen}](https://www.blockchain.com/btc/block/#{seen}),",
-        "while the latest we've seen was [#{latest}](https://www.blockchain.com/btc/block/#{latest});",
+        "while the latest is [#{latest}](https://www.blockchain.com/btc/block/#{latest});",
         'most probably there is something wrong with [Sibit](https://github.com/yegor256/sibit), please check'
       )
     end
