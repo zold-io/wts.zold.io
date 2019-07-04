fe61e77abf58695ccf3a32d70bd0ca84e70ab4b2
Yegor Bugayenko <yegor256@gmail.com>
2019-07-04 12:54:07 +0300

    formatting

--- objects/tracked_job.rb
+++ objects/tracked_job.rb
@@ -35,9 +35,9 @@ class WTS::TrackedJob
     @jobs.update(jid, 'OK')
   rescue Zold::Fetch::NotFound => e
     @jobs.result(jid, 'error_message', e.message)
-    @jobs.update(jid, e.message + '; most probably your wallet is lost by Zold network; \
+    @jobs.update(jid, e.message + "; most probably your wallet is lost by Zold network; \
 it is recommended to log in to wts.zold.io and click Restart at the top menu; a new wallet \
-will be created and this error will go away')
+will be created and this error will go away")
   rescue StandardError => e
     @jobs.result(jid, 'error_message', e.message)
     @jobs.update(jid, Backtrace.new(e).to_s)
