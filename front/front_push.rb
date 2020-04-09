805fb8a9ab3c92eed4282e2201620118e336dd56
Yegor Bugayenko <yegor256@gmail.com>
2020-04-09 19:06:44 +0300

    #346 /push fixed

--- front/front_push.rb
+++ front/front_push.rb
@@ -18,6 +18,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require 'zold/commands/create'
+
 get '/push' do
   features('push')
   haml :push, layout: :layout, locals: merged(
@@ -29,6 +31,15 @@ get '/do-push' do
   features('push')
   headers['X-Zold-Job'] = job do |_jid, log|
     log.info('Pushing the wallet to the network...')
+    unless user.wallet_exists?
+      key = user.item.key(keygap)
+      Tempfile.open do |f|
+        IO.write(f, OpenSSL::PKey::RSA.new(key.to_s).public_key.to_pem)
+        Zold::Create.new(wallets: settings.wallets, remotes: settings.remotes, log: settings.log).run(
+          ['create', user.item.id.to_s, "--network=#{network}", "--public-key=#{f.path}"]
+        )
+      end
+    end
     ops(log: log).push
   end
   flash('/', 'The wallet will be pushed now...')
