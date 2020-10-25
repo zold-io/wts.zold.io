d854bbfd56be4b844716a244f4b972d6bd62c149
yegor256 <yegor256@gmail.com>
2020-10-25 10:51:28 +0300

    #379 zc replaced by zold

--- assets/js/zld_to_btc.js
+++ assets/js/zld_to_btc.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2018-2020 Zerocracy, Inc.
+ * Copyright (c) 2018-2020 Zold
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the 'Software'), to deal
