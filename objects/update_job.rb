4b4dab909dda245883317290457e7329b811fbdd
yegor256 <yegor256@gmail.com>
2021-05-17 07:29:53 +0300

    #400 more logs

--- objects/update_job.rb
+++ objects/update_job.rb
@@ -22,7 +22,7 @@ require 'zold/log'
 require 'zold/commands/remote'
 
 #
-# Job that update remotes before and after.
+# Job that updates remotes before letting the original Job do the work.
 #
 class WTS::UpdateJob
   def initialize(job, remotes, log: Zold::Log::NULL, network: 'test')
@@ -36,6 +36,7 @@ class WTS::UpdateJob
     cmd = Zold::Remote.new(remotes: @remotes, log: @log)
     args = ['remote', "--network=#{@network}"]
     if @network == Zold::Wallet::MAINET
+      @log.info('Updating remotes before running the job...')
       cmd.run(args + ['trim'])
       cmd.run(args + ['masters'])
       cmd.run(args + ['reset']) if @remotes.all.empty?
