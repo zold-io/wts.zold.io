63a29cbc70eb31a6150c80f369fdcdf434f9c80b
davvd <davvd@zold.io>
2022-12-04 15:56:37 +0300

    #415 on pay too

--- objects/ops.rb
+++ objects/ops.rb
@@ -176,7 +176,16 @@ most probably you just have to RESTART your wallet"
       File.write(f, @item.key(keygap))
       @log.info("Paying #{amount} from #{id} to #{bnf}...")
       Zold::Pay.new(wallets: @wallets, remotes: @remotes, copies: @copies, log: @log).run(
-        ['pay', "--network=#{@network}", '--private-key=' + f.path, id.to_s, bnf.to_s, "#{amount.to_i}z", details]
+        [
+          'pay',
+          "--network=#{@network}",
+          '--ignore-score-weakness',
+          '--private-key=' + f.path,
+          id.to_s,
+          bnf.to_s,
+          "#{amount.to_i}z",
+          details
+        ]
       )
     end
     @log.info("Paid #{amount} from #{id} to #{bnf} in #{Zold::Age.new(start)} #{details.inspect}: #{txn.to_text}")
