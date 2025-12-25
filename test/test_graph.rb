417c41886297b3df5a50317063cb7cab0988d42a
Yegor Bugayenko <yegor256@gmail.com>
2025-12-25 08:27:02 +0300

    Add docker setup to GitHub workflows.

--- test/test_graph.rb
+++ test/test_graph.rb
@@ -9,18 +9,18 @@ class WTS::GraphTest < Minitest::Test
   def test_renders_svg
     WebMock.allow_net_connect!
     ticks = WTS::Ticks.new(t_pgsql, log: t_log)
-    ticks.add('Price' => 1, 'time' => tme(-1))
-    ticks.add('Price' => 3, 'time' => tme(-2))
-    ticks.add('Price' => 2, 'time' => tme(-10))
-    ticks.add('Price' => 1.5, 'time' => tme(-14))
-    ticks.add('Price' => 1.2, 'time' => tme(-50))
+    ticks.add('Price' => 1, 'time' => msec(-1))
+    ticks.add('Price' => 3, 'time' => msec(-2))
+    ticks.add('Price' => 2, 'time' => msec(-10))
+    ticks.add('Price' => 1.5, 'time' => msec(-14))
+    ticks.add('Price' => 1.2, 'time' => msec(-50))
     FileUtils.mkdir_p('target')
     File.write('target/graph.svg', WTS::Graph.new(ticks, log: t_log).svg(['Price'], 1, 0))
   end
 
   private
 
-  def tme(days)
+  def msec(days)
     ((Time.now.to_f + (days * 24 * 60 * 60)) * 1000).to_i
   end
 end
